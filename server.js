7d5a6062d8d97ab020e0a35692202e50b5cabe17
Victor Evdokimov <ext_rem@list.ru>
2021-03-08 20:37:20 +0300

    Add two rest methods.     GET /cart - show cart list     Delete /card - delet product from cart

--- server.js
+++ server.js
@@ -12,12 +12,32 @@ api.get('/goods-list', (req, res) => {
   res.json(data);
 });
 
+api.get('/cart', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./cart.json'));
+  res.json(data);
+});
+
 api.post('/cart', (req, res) => {
   const cart = JSON.parse(fs.readFileSync('./cart.json'));
   cart.push(req.body);
   console.log(cart);
   fs.writeFileSync('./cart.json', JSON.stringify(cart));
   res.send('ok');
+})
+
+api.delete('/cart', (req, res, next) => {
+  const cart = JSON.parse(fs.readFileSync('./cart.json'));
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id === req.body.id) {
+      cart.splice(i, 1);
+        fs.writeFileSync('./cart.json', JSON.stringify(cart));
+        res.send('ok');
+        return;
+    } 
+  }
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 })
 
 api.listen(3000, () => {
